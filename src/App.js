9b867ca148fad522fcd6488b55b5b9ba53d484a6
IleniaTedde <i.tedde@xister.com>
2022-06-10 17:31:38 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>pages/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          ciao
-        </a>
-      </header>
-    </div>
+    <>
+      <header className="App-header"> HEADER </header>
+        <div className="content">
+        luxuryDreamService_website
+        </div>
+        <footer className="App-footer">FOOTER</footer>
+        </>
   );
 }
 
