5a87fd899f09351f9dac43885d42fb51a93f0588
IleniaTedde <i.tedde@xister.com>
2022-06-22 11:53:40 +0200

    font

--- src/App.js
+++ src/App.js
@@ -38,6 +38,12 @@ function App() {
   }
   return (
     <>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" />
+
+
    {locale && 
     <Router>
         <Switch>    
